d87a2dde014bcebc69b94fe9a13407ab100ceb36
T-Gamer <1985michel>
2022-04-15 20:11:20 -0300

    incluindo módulo 6

--- vue-cli/projeto-simples/src/main.js
+++ vue-cli/projeto-simples/src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+
+  //render é o método é h é a função que cria o elemento
+  render: h => h(App)
 }).$mount('#app')
